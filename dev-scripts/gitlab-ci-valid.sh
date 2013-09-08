31b56d2bb6f708cceccb37c7e89fc2071772521a
Sebastien Valat <sebastien.valat.dev@orange.fr>
2013-09-08 16:59:09 +0200

    * add gitlab-ci validation script

---
+++ dev-scripts/gitlab-ci-valid.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#!/bin/bash
+
+if [ ! -f dev-scripts/gitlab-ci-valid.sh ]; then
+        echo "Must be executed from project root directory" 1>&2
+        exit 1
+fi
+
+set -e
+set -x
+
+rm -rf build
+mkdir -p build
+cd build
+../configure --enable-debug
+make
+make test
