20418a648f05fda79749d8564ff57aa5d4837c2f
Sebastien Valat <sebastien.valat.dev@orange.fr>
2014-11-02 18:45:18 +0100

    * Fix the nodeJS parsor tests to take in account the IR structured tree

--- src/compiler-v2/latex-parser-test.js
+++ src/compiler-v2/latex-parser-test.js
@@ -97,11 +97,69 @@ var testData =  {
 };
 
 /******************************************************************************/
+var longObjectRepr = {
+	"a":                  {"content":[{"name":"a"}]},
+	"\\pi":               {"content":[{"name":"\\pi"}]},
+	"33":                 {"content":[{"name":"33"}]},
+	"33.5":               {"content":[{"name":"33.5"}]},
+	"\\cos":              {"content":[{"name":"\\cos"}]},
+	"\\cos{i}":           {"content":[{"name":"\\cos","parameters":[{"content":[{"name":"i"}]}]}]},
+	"\\cos{i}{j}":        {"content":[{"name":"\\cos","parameters":[{"content":[{"name":"i"}]},{"content":[{"name":"j"}]}]}]},
+	"\\cos{i}{j}{k}":     {"content":[{"name":"\\cos","parameters":[{"content":[{"name":"i"}]},{"content":[{"name":"j"}]},{"content":[{"name":"k"}]}]}]},
+	"a^b":                {"content":[{"name":"a","exponents":[{"content":[{"name":"b"}]}]}]},
+	"a^{b}":              {"content":[{"name":"a","exponents":[{"content":[{"name":"b"}]}]}]},
+	"a^{b,c}":            {"content":[{"name":"a","exponents":[{"content":[{"name":"b"}]},{"content":[{"name":"c"}]}]}]},
+	"a^{b,c,d}":          {"content":[{"name":"a","exponents":[{"content":[{"name":"b"}]},{"content":[{"name":"c"}]},{"content":[{"name":"d"}]}]}]},
+	"a_i":                {"content":[{"name":"a","indices":[{"content":[{"name":"i"}]}]}]},
+	"a_{i}":              {"content":[{"name":"a","indices":[{"content":[{"name":"i"}]}]}]},
+	"a_{i,j}":            {"content":[{"name":"a","indices":[{"content":[{"name":"i"}]},{"content":[{"name":"j"}]}]}]},
+	"a_{i,j,k}":          {"content":[{"name":"a","indices":[{"content":[{"name":"i"}]},{"content":[{"name":"j"}]},{"content":[{"name":"k"}]}]}]},
+	"(a)":                {"content":[{"name":"()","groupContent":{"content":[{"name":"a"}]}}]},
+	"(a+b)":              {"content":[{"name":"()","groupContent":{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]}}]},
+	"(a+b)*(c+d)":        {"content":[{"name":"()","groupContent":{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]}},{"name":"*"},{"name":"()","groupContent":{"content":[{"name":"c"},{"name":"+"},{"name":"d"}]}}]},
+	"(a+b)^33":           {"content":[{"name":"()","groupContent":{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]},"exponents":[{"content":[{"name":"33"}]}]}]},
+	"(a+b)_{i,j}":        {"content":[{"name":"()","groupContent":{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]},"indices":[{"content":[{"name":"i"}]},{"content":[{"name":"j"}]}]}]},
+	"a^(a+b)":            {"content":[{"name":"a","exponents":[{"content":[{"name":"()","groupContent":{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]}}]}]}]},
+	"3 a":                {"content":[{"name":"3"},{"name":"[*]"},{"name":"a"}]},
+	"3 * a":              {"content":[{"name":"3"},{"name":"*"},{"name":"a"}]},
+	"ab":                 {"content":[{"name":"a"},{"name":"[*]"},{"name":"b"}]},
+	"3a":                 {"content":[{"name":"3"},{"name":"[*]"},{"name":"a"}]},
+	"3(a+b)":             {"content":[{"name":"3"},{"name":"[*]"},{"name":"()","groupContent":{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]}}]},
+	"a+b-c*d/e":          {"content":[{"name":"a"},{"name":"+"},{"name":"b"},{"name":"-"},{"name":"c"},{"name":"*"},{"name":"d"},{"name":"/"},{"name":"e"}]},
+	"a^\\cos{5}":         {"content":[{"name":"a","exponents":[{"content":[{"name":"\\cos","parameters":[{"content":[{"name":"5"}]}]}]}]}]},
+	"a^\\cos{5}{6}":      {"content":[{"name":"a","exponents":[{"content":[{"name":"\\cos","parameters":[{"content":[{"name":"5"}]},{"content":[{"name":"6"}]}]}]}]}]},
+	"a^{b^2,c^2}":        {"content":[{"name":"a","exponents":[{"content":[{"name":"b","exponents":[{"content":[{"name":"2"}]}]}]},{"content":[{"name":"c","exponents":[{"content":[{"name":"2"}]}]}]}]}]},
+	"\\cos{a+b}":         {"content":[{"name":"\\cos","parameters":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]}]}]},
+	"\\cos{a+b}{d+e}":    {"content":[{"name":"\\cos","parameters":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]},{"content":[{"name":"d"},{"name":"+"},{"name":"e"}]}]}]},
+	"\\cos{a+b}{d+e}{h+j}":{"content":[{"name":"\\cos","parameters":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]},{"content":[{"name":"d"},{"name":"+"},{"name":"e"}]},{"content":[{"name":"h"},{"name":"+"},{"name":"j"}]}]}]},
+	"a^{a+b}":            {"content":[{"name":"a","exponents":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]}]}]},
+	"a^{a+b,c+d}":        {"content":[{"name":"a","exponents":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]},{"content":[{"name":"c"},{"name":"+"},{"name":"d"}]}]}]},
+	"a^{a+b,c+d,e+f}":    {"content":[{"name":"a","exponents":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]},{"content":[{"name":"c"},{"name":"+"},{"name":"d"}]},{"content":[{"name":"e"},{"name":"+"},{"name":"f"}]}]}]},
+	"a_{a+b}":            {"content":[{"name":"a","indices":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]}]}]},
+	"a_{a+b,c+d}":        {"content":[{"name":"a","indices":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]},{"content":[{"name":"c"},{"name":"+"},{"name":"d"}]}]}]},
+	"a_{a+b,c+d,e+f}":    {"content":[{"name":"a","indices":[{"content":[{"name":"a"},{"name":"+"},{"name":"b"}]},{"content":[{"name":"c"},{"name":"+"},{"name":"d"}]},{"content":[{"name":"e"},{"name":"+"},{"name":"f"}]}]}]},
+	"a^{b}_{c}":          {"content":[{"name":"a","exponents":[{"content":[{"name":"b"}]}],"indices":[{"content":[{"name":"c"}]}]}]},
+	"a^b_c":              {"content":[{"name":"a","exponents":[{"content":[{"name":"b"}]}],"indices":[{"content":[{"name":"c"}]}]}]},
+	"a^bc_d":             {"content":[{"name":"a","exponents":[{"content":[{"name":"b"}]}]},{"name":"[*]"},{"name":"c","indices":[{"content":[{"name":"d"}]}]}]},
+	"-33":                {"content":[{"name":"[-]"},{"name":"33"}]},
+	"a*(-5)":             {"content":[{"name":"a"},{"name":"*"},{"name":"()","groupContent":{"content":[{"name":"[-]"},{"name":"5"}]}}]},
+	"-a*5":               {"content":[{"name":"[-]"},{"name":"a"},{"name":"*"},{"name":"5"}]},
+	"-a*\\cos{a}{b}^6":   {"content":[{"name":"[-]"},{"name":"a"},{"name":"*"},{"name":"\\cos","parameters":[{"content":[{"name":"a"}]},{"content":[{"name":"b"}]}],"exponents":[{"content":[{"name":"6"}]}]}]},
+	"a*(-\\cos{a}{b}^6)":  {"content":[{"name":"a"},{"name":"*"},{"name":"()","groupContent":{"content":[{"name":"[-]"},{"name":"\\cos","parameters":[{"content":[{"name":"a"}]},{"content":[{"name":"b"}]}],"exponents":[{"content":[{"name":"6"}]}]}]}}]},
+	"a*(-\\cos{-a}{-b}^-6)":{"content":[{"name":"a"},{"name":"*"},{"name":"()","groupContent":{"content":[{"name":"[-]"},{"name":"\\cos","parameters":[{"content":[{"name":"[-]"},{"name":"a"}]},{"content":[{"name":"[-]"},{"name":"b"}]}],"exponents":[{"content":[{"name":"[-]"},{"name":"6"}]}]}]}}]},
+	"a b":                {"content":[{"name":"a"},{"name":"[*]"},{"name":"b"}]},
+	"a  b":               {"content":[{"name":"a"},{"name":"[*]"},{"name":"b"}]},
+	"a		b":           {"content":[{"name":"a"},{"name":"[*]"},{"name":"b"}]},
+	"a	 b":              {"content":[{"name":"a"},{"name":"[*]"},{"name":"b"}]}
+};
+
+/******************************************************************************/
 function runTest(test,def)
 {
 // 	console.log(i);
 	var res = JSON.stringify(parser.parse(def.input));
-	test.equal(res,def.output,"Get error in parsor !");
+	var ref = JSON.stringify(longObjectRepr[def.input]);
+	test.equal(res,ref,"Get error in parsor !");
 	test.done();
 }
 
